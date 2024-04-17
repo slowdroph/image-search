7639c29ce4403ab46396d0790729069322013bba
caique <caiqueestrela538@gmail.com>
2024-04-16 22:36:54 -0300

    responsive

--- src/ImageList.jsx
+++ src/ImageList.jsx
@@ -2,7 +2,7 @@ import styles from "./ImageList.module.css";
 
 function ImageList({ imagem, onSelectImage }) {
     return (
-        <li onClick={() => onSelectImage(imagem.id)}>
+        <li className={styles.list} onClick={() => onSelectImage(imagem.id)}>
             <img
                 className={styles.img}
                 src={imagem.largeImageURL}
