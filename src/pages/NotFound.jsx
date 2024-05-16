949fa96ae5d0b7f91ea6b695dded2d21cc4c4c21
caique <caiqueestrela538@gmail.com>
2024-05-15 21:37:52 -0300

    Netlify route fixed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom"
-
 function NotFound() {
     return (
-        <div>
-            page not found
-            <Link to='search' >ser</Link>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh",
+            }}
+        >
+            <img src="https://avatars.mds.yandex.net/i?id=e628b58c75f7e26fd69b6af7dd3b42188aa3aa95-10188656-images-thumbs&n=13" />
         </div>
-    )
+    );
 }
 
-export default NotFound
+export default NotFound;
