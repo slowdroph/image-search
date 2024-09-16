6c88f9be8eb8a3c14ce83ac29f05b30adec77a62
caique <caiqueestrela538@gmail.com>
2024-09-15 22:23:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
     const [favorites, setFavorite] = useState([]);
-    
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Search />} />
                     <Route
                         index
                         path="search"
-                        element={<Search favorites={favorites} setFavorite={setFavorite}/>}
+                        element={
+                            <Search
+                                favorites={favorites}
+                                setFavorite={setFavorite}
+                            />
+                        }
                     />
                     <Route
                         path="favorites"
-                        element={<PageFavorites favorites={favorites} setFavorite={setFavorite}/>}
+                        element={
+                            <PageFavorites
+                                favorites={favorites}
+                                setFavorite={setFavorite}
+                            />
+                        }
                     />
                     <Route path="details" element={<PageDetails />} />
                     <Route path="*" element={<NotFound />} />
